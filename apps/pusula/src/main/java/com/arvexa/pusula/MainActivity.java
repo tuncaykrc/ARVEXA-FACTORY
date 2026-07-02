9db79a8f4f0e5a63e554f12593efbd50073bd8dd
tuncaykrc <m.tuncaykaraca@gmail.com>
2026-07-02 22:37:46 +0300

    Update MainActivity.java

--- apps/pusula/src/main/java/com/arvexa/pusula/MainActivity.java
+++ apps/pusula/src/main/java/com/arvexa/pusula/MainActivity.java
@@ -1,1 +1,40 @@
-// Pusula'nın beyni
+package com.arvexa.pusula;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor magnetometer;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        textView = new TextView(this);
+        setContentView(textView);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float degree = event.values[0];
+        textView.setText("Pusula Açısı: " + degree);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+}
